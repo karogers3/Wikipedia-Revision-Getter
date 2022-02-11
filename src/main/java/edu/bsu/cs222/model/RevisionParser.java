4c9e454a01b1c7db0301a0a1585a98787c7a10cf
Kameron Rogers <karogers3@bsu.edu>
2022-02-11 17:09:06 -0500

    Turn InputStream into a string

--- src/main/java/edu/bsu/cs222/model/RevisionParser.java
+++ src/main/java/edu/bsu/cs222/model/RevisionParser.java
@@ -7,13 +7,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class RevisionParser {
 
     public String parse(InputStream testDataStream) throws IOException {
-        JSONArray result = (JSONArray) JsonPath.read(testDataStream, "$..user");
+        Scanner scanner = new Scanner(testDataStream);
+        String jsonString = scanner.nextLine();
+        JSONArray result = (JSONArray) JsonPath.read(jsonString, "$..user");
+        JSONArray result2 = (JSONArray) JsonPath.read(jsonString, "$..timestamp");
         List<Revision> revisionList = new ArrayList<Revision>();
-        Revision revision = new Revision(null, result.get(0).toString());
+        Revision revision = new Revision(result2.get(0).toString(), result.get(0).toString());
         return revision.getUsername();
     }
 }
