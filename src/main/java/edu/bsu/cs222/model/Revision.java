18fa9112eea640f3e9f6ac9d91aa0b0086584a6f
Kameron Rogers <karogers3@bsu.edu>
2022-02-11 12:56:39 -0500

    Add Revision class and refactor RevisionParser

---
+++ src/main/java/edu/bsu/cs222/model/Revision.java
@@ -0,0 +1,20 @@
+package edu.bsu.cs222.model;
+
+public class Revision {
+    private final String timestamp;
+    private final String username;
+
+    public Revision(String timestamp, String username) {
+        this.timestamp=timestamp;
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+
+}
--- src/main/java/edu/bsu/cs222/model/RevisionParser.java
+++ src/main/java/edu/bsu/cs222/model/RevisionParser.java
@@ -5,11 +5,15 @@ import net.minidev.json.JSONArray;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RevisionParser {
 
     public String parse(InputStream testDataStream) throws IOException {
         JSONArray result = (JSONArray) JsonPath.read(testDataStream, "$..user");
-        return result.get(0).toString();
+//        List<Revision> revisionList = new ArrayList<Revision>;
+        Revision revision = new Revision(null, result.get(0).toString());
+        return revision.getUsername();
     }
 }
