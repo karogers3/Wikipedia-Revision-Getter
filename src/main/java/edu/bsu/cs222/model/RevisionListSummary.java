57855a8977c2c7dc9275af7f85868478a1044f39
Kameron Rogers <karogers3@bsu.edu>
2022-02-13 15:46:38 -0500

    Add comment clarifying sizeOfLongestUsername in RevisionListSummary.

--- src/main/java/edu/bsu/cs222/model/RevisionListSummary.java
+++ src/main/java/edu/bsu/cs222/model/RevisionListSummary.java
@@ -11,6 +11,7 @@ public class RevisionListSummary {
         String userNameFormat = "%-" + maxLength + "s";
         for (Revision revision : revisionList) {
             // length of "Timestamp: " + "yyyy.MM.ddTHH:mm:ssZ" = 31
+            // Largest digit will be 30, which is why there is a 2 in "%-2d."
             String revisionString = String.format("\n%-2d. " + userNameFormat + " %-31s\n", i, "User: " +
                             revision.getUsername(), "Timestamp: " + revision.getTimestamp());
             stringBuilder.append(revisionString);
