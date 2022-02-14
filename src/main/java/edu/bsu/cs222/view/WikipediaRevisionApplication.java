6b027d86c0b5790269eb4fe03d71b74416ba8025
paul <nplian@bsu.edu>
2022-02-14 01:11:42 -0500

    Making Scroll bar form WikipediaRevisionApplication so we can scroll it to see all the infomation.

--- src/main/java/edu/bsu/cs222/view/WikipediaRevisionApplication.java
+++ src/main/java/edu/bsu/cs222/view/WikipediaRevisionApplication.java
@@ -7,7 +7,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -18,17 +20,29 @@ import java.util.concurrent.Executors;
 
 public class WikipediaRevisionApplication extends Application {
     private final Executor executor = Executors.newSingleThreadExecutor();
-    private final Label label = new Label("Is it?");
+    private final Label label = new Label("");
     private final Button button = new Button("Search");
     private final TextField textField = new TextField();
     private String display;
 
-    //TODO: Make this a JavaFX GUI!
+
+
+        
 
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setScene(new Scene(makeUI()));
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 450, 500);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Wiki");
+        ScrollPane scroll = new ScrollPane();
+        root.setCenter(scroll);
+        scroll.setContent(makeUI());
+        scroll.setPannable(true);
+        scroll.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        scroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
         primaryStage.show();
     }
 
@@ -74,8 +88,8 @@ public class WikipediaRevisionApplication extends Application {
             });
         });
 
+
         VBox vbox = new VBox();
-        vbox.setPrefSize(350, 600);
         vbox.getChildren().addAll(
                 textField,
                 button,
